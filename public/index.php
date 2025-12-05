3cc0025b97e1c14941f4b7b1441e9927d7bd81ba
QDat18 <hoangquangdat182005@gmail.com>
2025-12-05 23:56:31 +0700

    gan finall

--- public/index.php
+++ public/index.php
@@ -6,15 +6,15 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Maintenance mode
-if (file_exists(__DIR__.'/../Project_Final/storage/framework/maintenance.php')) {
-    require __DIR__.'/../Project_Final/storage/framework/maintenance.php';
+if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
+    require __DIR__.'/../storage/framework/maintenance.php';
 }
 
 // Autoload
-require __DIR__ . '/../Project_Final/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel
-$app = require_once __DIR__ . '/../Project_Final/bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
