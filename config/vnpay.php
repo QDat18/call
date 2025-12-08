e6fb6967c60c47d5a7385e2ad0b22ce56f345782
QDat18 <161273032+QDat18@users.noreply.github.com>
2025-12-08 12:48:16 +0700

    Delete config/vnpay.php

--- config/vnpay.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'vnp_TmnCode' => env('VNPAY_TMN_CODE', ''),
-    'vnp_HashSecret' => env('VNPAY_HASH_SECRET', ''),
-    'vnp_Url' => env('VNPAY_URL', 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'),
-    'vnp_ReturnUrl' => env('VNPAY_RETURN_URL', '/donation/vnpay-return'),
-];
