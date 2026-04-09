ef1070a93e23b5b62928c81b1b7fbc2546e32514
QDat18 <161273032+QDat18@users.noreply.github.com>
2026-04-09 16:07:43 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -32,10 +32,11 @@ return [
             'strict' => true,
             'engine' => null,
 
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            'options' => extension_loaded('pdo_mysql') ? [
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+            ] : [],
+            
+            'sslmode' => env('DB_SSLMODE', 'disable'),
         ],
 
         'mariadb' => [
